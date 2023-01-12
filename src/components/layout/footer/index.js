ada84ac75674ceee1bb68f76ddf04eb3d3285548
yash.parmar <yash.parmar@agileinfoways.com>
2023-01-12 13:54:45 +0530

    first commit

---
+++ src/components/layout/footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <>
+      <h2 style={{ textAlign: "center" }}>Footer</h2>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/container/App.js
@@ -0,0 +1,29 @@
+import React, { Suspense } from "react";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./routes";
+import { ErrorBoundary } from "../Error/index";
+// import axios from "axios";
+// import { Provider } from "react-redux";
+// import { store } from "../Redux/store";
+// import Loader from "../components/common/loader";
+// import { setupAxios } from "../utils";
+const { PUBLIC_URL } = process.env.dev;
+
+// setupAxios(axios, store);
+
+const AppContainer = () => (
+  <ErrorBoundary>
+    {/* <Provider store={store}> */}
+    {/* <Suspense fallback={<Loader isSuspense />}> */}
+    <Suspense>
+      {/* <Loader>  */}
+      <BrowserRouter basename={PUBLIC_URL}>
+        <Routes />
+      </BrowserRouter>
+      {/* </Loader> */}
+    </Suspense>
+    {/* </Provider> */}
+  </ErrorBoundary>
+);
+
+export default AppContainer;
